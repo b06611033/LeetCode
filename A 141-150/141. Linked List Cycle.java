b8204b7e783c42a220e2b238ee3ef8cf33e8760f
b06611033 <52776608+b06611033@users.noreply.github.com>
2022-06-28 23:07:52 -0700

    Update 141. Linked List Cycle.java

--- A 141-150/141. Linked List Cycle.java
+++ A 141-150/141. Linked List Cycle.java
@@ -1,1 +1,23 @@
-
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        HashSet<ListNode> set = new HashSet<ListNode>();
+        while (head != null) {
+            if (set.contains(head)) return true;
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
