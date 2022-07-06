af73687147fe0274b71a37ffc4a3bf259a59417a
b06611033 <52776608+b06611033@users.noreply.github.com>
2022-07-05 22:02:48 -0700

    Update 212. Word Search II.java

--- A 211-220/212. Word Search II.java
+++ A 211-220/212. Word Search II.java
@@ -1,7 +1,6 @@
-// exceed time limit in large cases
 class Solution {
     List<String> answer;
-    Set<String> dictionary;
+    Trie dictionary;
     int m;
     int n;
     int longest;
@@ -10,81 +9,109 @@ class Solution {
         n = board[0].length;
         longest = 0;
         answer = new ArrayList<String>();
-        dictionary = new HashSet<String>();
+        dictionary = new Trie();
         for (String s: words) {
-            dictionary.add(s);
+            dictionary.insert(s);
             if (s.length() > longest) longest = s.length();
         }
+        boolean[][] visited = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                boolean[][] traversed = new boolean[m][n];
-                traversed[i][j] = true;
-                String s = new String();
-                s += board[i][j];
-                if (dictionary.contains(s)) {
-                    answer.add(s);
-                    dictionary.remove(s);
-                } 
-                find(board, i, j, traversed, s, 2);
+                find(board, i, j, visited, "", 0);
             }
         }
         return answer; 
     }
-    private void find(char[][] board, int row, int col, boolean[][] traversed, String s, int len) {
+    private void find(char[][] board, int row, int col, boolean[][] visited, String s, int len) {
+        if (row < 0 || col < 0 || row >= m || col >= n) return;
+        if (visited[row][col]) return;
+        String newString = new String(s);
+        newString += board[row][col];
+        len++;
         if (len > longest) return;
-        if (row < m-1 && !traversed[row+1][col]) {
-            boolean[][] newTraversed = new boolean[m][n];
-            deepCopy(traversed, newTraversed);
-            newTraversed[row+1][col] = true;
-            String newString = new String(s);
-            newString += board[row+1][col];
-            if (dictionary.contains(newString)) {
+        if (!dictionary.startsWith(newString)) return;
+        if (dictionary.search(newString)) {
                 answer.add(newString);
                 dictionary.remove(newString);
-            } 
-            find(board, row+1, col, newTraversed, newString, len+1);
-        }
-        if (col < n-1 && !traversed[row][col+1]) {
-            boolean[][] newTraversed = new boolean[m][n];
-            deepCopy(traversed, newTraversed);
-            newTraversed[row][col+1] = true;
-            String newString = new String(s);
-            newString += board[row][col+1];
-            if (dictionary.contains(newString)) {
-                answer.add(newString);
-                dictionary.remove(newString);
-            } 
-            find(board, row, col+1, newTraversed, newString, len+1);
-        }
-        if (row > 0 && !traversed[row-1][col]) {
-            boolean[][] newTraversed = new boolean[m][n];
-            deepCopy(traversed, newTraversed);
-            newTraversed[row-1][col] = true;
-            String newString = new String(s);
-            newString += board[row-1][col];
-            if (dictionary.contains(newString)) {
-                answer.add(newString);
-                dictionary.remove(newString);
-            } 
-            find(board, row-1, col, newTraversed, newString, len+1);
-        }
-        if (col > 0 && !traversed[row][col-1]) {
-            boolean[][] newTraversed = new boolean[m][n];
-            deepCopy(traversed, newTraversed);
-            newTraversed[row][col-1] = true;
-            String newString = new String(s);
-            newString += board[row][col-1];
-            if (dictionary.contains(newString)) {
-                answer.add(newString);
-                dictionary.remove(newString);
-            } 
-            find(board, row, col-1, newTraversed, newString, len+1);
-        }
+        } 
+        visited[row][col] = true;
+        find(board, row+1, col, visited, newString, len);
+        find(board, row, col+1, visited, newString, len);
+        find(board, row-1, col, visited, newString, len);
+        find(board, row, col-1, visited, newString, len);
+        visited[row][col] = false;
         return;
     }
-    private void deepCopy(boolean[][] original, boolean[][] copy) {
-        for (int i = 0; i < original.length; i++) {
-            copy[i] = original[i].clone();
+}
+class Trie {
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node.contain(word.charAt(i))) {
+                TrieNode newNode = new TrieNode();
+                node.put(word.charAt(i), newNode);
+                node = newNode;
+            }
+            else node = node.get(word.charAt(i));
         }
+        node.setEnd();
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node.contain(word.charAt(i))) node = node.get(word.charAt(i));
+            else return false;
+        }
+        return node.isEnd();
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (node.contain(prefix.charAt(i))) node = node.get(prefix.charAt(i));
+            else return false;
+        }
+        return true;
+    }
+    
+    public void remove(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node.contain(word.charAt(i))) node = node.get(word.charAt(i));
+            else return;
+        }
+        node.removeEnd();
+    }
+}
+
+class TrieNode {
+    private final int R = 26;
+    private boolean isEnd;
+    private TrieNode[] child;
+    public TrieNode() {
+        child = new TrieNode[R];
+    }
+    public void put(char c, TrieNode n) {
+        child[c - 'a'] = n;
+    }
+    public TrieNode get(char c) {
+        return child[c - 'a'];
+    }
+    public boolean contain(char c) {
+        return child[c - 'a'] != null;
+    }
+    public boolean isEnd() {
+        return isEnd;
+    }
+    public void setEnd() {
+        isEnd = true;
+    }
+    public void removeEnd() {
+        isEnd = false;
     }
 }
